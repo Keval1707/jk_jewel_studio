1b6886b453b3c9610407c895258fd5adc6029da8
Keval1707 <kevalsatani6514@gmail.com>
2025-05-28 16:16:35 +0530

    test

--- jkjewelstudio/src/utils/api.js
+++ jkjewelstudio/src/utils/api.js
@@ -6,6 +6,12 @@ const api = axios.create({
   baseURL: API_BASE_URL,
 });
 
+// Log every request URL
+api.interceptors.request.use((config) => {
+  console.log("API Call:", config.baseURL + config.url);
+  return config;
+});
+
 export const fetchProducts = () => api.get("/product");
 export const fetchProductById = (id) => api.get(`/product/${id}`);
 
@@ -14,6 +20,5 @@ export const fetchCategoryById = (id) => api.get(`/category/${id}`);
 
 export const postPlaceOrder = (orderData) => api.post("/order/place", orderData);
 export const contact = (contactData) => api.post("/contact", contactData);
-
 
 export default api;
